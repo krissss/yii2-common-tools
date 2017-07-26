27568cccb0bb0db2e65113f8a26d4a06e733c12f
kriss <462679766@qq.com>
2017-07-26 12:00:17 +0800

    fix: modify default config pagination set

--- components/rest/ActiveDataProvider.php
+++ components/rest/ActiveDataProvider.php
@@ -2,15 +2,30 @@
 
 namespace kriss\components\rest;
 
+use Yii;
+use yii\data\Pagination;
+
 class ActiveDataProvider extends \yii\data\ActiveDataProvider
 {
-    public function init()
+    /**
+     * 修改：修改 pagination 的默认值，且不影响原应用
+     * @inheritdoc
+     */
+    public function setPagination($value)
     {
-        parent::init();
-        $this->pagination = [
-            'pageParam' => 'page',
-            'pageSizeParam' => 'per_page',
-            'validatePage' => false,
-        ];
+        if (is_array($value)) {
+            $value = array_merge([
+                'pageParam' => 'page',
+                'pageSizeParam' => 'per_page',
+                'validatePage' => false,
+            ], $value);
+            $config = ['class' => Pagination::className()];
+            if ($this->id !== null) {
+                $config['pageParam'] = $this->id . '-page';
+                $config['pageSizeParam'] = $this->id . '-per-page';
+            }
+            $value = Yii::createObject(array_merge($config, $value));
+        }
+        parent::setPagination($value);
     }
 }
