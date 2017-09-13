b4137338a62f78b9bdcd2d901b0df9f2ec8c03ab
kriss <462679766@qq.com>
2017-09-13 21:48:55 +0800

    fix: kartik-v/yii2-field-range updated to 1.3.2 above

--- widgets/DateRangeBase.php
+++ widgets/DateRangeBase.php
@@ -3,8 +3,6 @@
 namespace kriss\widgets;
 
 use kartik\field\FieldRange;
-use kartik\helpers\Html;
-use yii\helpers\ArrayHelper;
 
 class DateRangeBase extends FieldRange
 {
@@ -28,25 +26,5 @@ class DateRangeBase extends FieldRange
             $this->widgetOptions2['type'] = $this->displayType;
         }
         parent::init();
-    }
-
-    public function renderWidget()
-    {
-        Html::addCssClass($this->options, 'kv-field-range');
-
-        Html::addCssClass($this->options, 'input-group');
-        $tag = ArrayHelper::remove($this->separatorOptions, 'tag', 'span');
-        $widget = isset($this->form) ? $this->getFormInput() : $this->getInput(1) .
-            Html::tag($tag, $this->separator, $this->separatorOptions) . $this->getInput(2);
-        $widget = Html::tag('div', $widget, $this->options);
-
-        $widget = Html::tag('div', $widget, $this->widgetContainer);
-        $error = Html::tag('div', '<div class="help-block"></div>', $this->errorContainer);
-
-        echo Html::tag('div', strtr($this->template, [
-            '{label}' => Html::label($this->label, null, $this->labelOptions),
-            '{widget}' => $widget,
-            '{error}' => $error
-        ]), $this->container);
     }
 }
