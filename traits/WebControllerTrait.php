a86d8c0b9403362e01e93dd6c50b66bb03720d57
kriss <462679766@qq.com>
2018-02-08 18:11:31 +0800

    feat: add config to controller

--- traits/WebControllerTrait.php
+++ traits/WebControllerTrait.php
@@ -15,29 +15,34 @@ trait WebControllerTrait
 {
     /**
      * remember current url for redirect
+     * @param false|\yii\web\Controller $controller
      */
-    public function rememberUrl()
+    public function rememberUrl($controller = false)
     {
+        $controller = $controller ?: $this;
         Yii::trace('remember current url', __METHOD__);
         /** @var $this Controller */
-        Url::remember('', get_class($this));
+        Url::remember('', get_class($controller));
     }
 
     /**
      * redirect to rememberUrl() remembered url or return the redirect url string
      * if request is ajax, return the redirect url string
      * otherwise, send redirect to rememberUrl() remembered
+     * @param false|\yii\web\Controller $controller
      * @return string|\yii\web\Response
      */
-    public function actionPreviousRedirect()
+    public function actionPreviousRedirect($controller = false)
     {
+        /** @var Controller $this */
+        $controller = $controller ?: $this;
+        $previous = Url::previous(get_class($controller));
         if (Yii::$app->request->isAjax && !Yii::$app->request->isPjax) {
-            Yii::trace('return previous url string: ' . Url::previous(get_class($this)), __METHOD__);
-            return Url::previous(get_class($this));
+            Yii::trace('return previous url string: ' . $previous, __METHOD__);
+            return $previous;
         } else {
-            Yii::trace('redirect previous url: ' . Url::previous(get_class($this)), __METHOD__);
-            /** @var $this Controller */
-            return $this->redirect(Url::previous(get_class($this)));
+            Yii::trace('redirect previous url: ' . $previous, __METHOD__);
+            return $controller->redirect($previous);
         }
     }
 
