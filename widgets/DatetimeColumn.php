077eb7e255babe66068d24116ac4bee9ad1f8c93
kriss <462679766@qq.com>
2018-07-11 16:29:15 +0800

    fix: 移除固定宽度

--- widgets/DatetimeColumn.php
+++ widgets/DatetimeColumn.php
@@ -6,8 +6,6 @@ class DatetimeColumn extends DataColumn
 {
     public $dateFormat = 'Y-m-d H:i:s';
 
-    public $width = '100px';
-
     public function init()
     {
         $this->format = ['datetime', 'php:' . $this->dateFormat];
