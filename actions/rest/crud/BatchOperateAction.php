dd9e7da2ebc2e0421209493c6e0bcdbad7b3b8f7
kriss <462679766@qq.com>
2018-07-27 14:01:07 +0800

    feat：增加后台批量操作 action

--- actions/rest/crud/BatchOperateAction.php
+++ actions/rest/crud/BatchOperateAction.php
@@ -12,6 +12,12 @@ class BatchOperateAction extends AbstractAction
      */
     public $idsAttribute = 'ids';
     /**
+     * ids 是否是数组，如果不是，会进行 explode 切割为数组
+     * @var bool
+     */
+    public $isIdsArr = false;
+    /**
+     * isIdsArr 为 false 时，用来切割字符串的分割符
      * @var string
      */
     public $explodeBy = ',';
@@ -23,12 +29,34 @@ class BatchOperateAction extends AbstractAction
 
     public function run()
     {
-        $ids = Yii::$app->request->post($this->idsAttribute);
+        $ids = $this->getRequestIds();
         if (!$ids) {
-            return ActionTools::restValidateError('ids 必传');
+            return ActionTools::restValidateError("{$this->idsAttribute} 必传");
         }
-        $idsArr = array_unique(array_filter(explode($this->explodeBy, $ids)));
+        $idsArr = $this->transIdsToArr($ids);
 
         return ActionTools::invokeClassMethod($this->controller, $this->doMethod, $idsArr);
+    }
+
+    /**
+     * @return array|mixed
+     */
+    protected function getRequestIds()
+    {
+        return Yii::$app->request->post($this->idsAttribute);
+    }
+
+    /**
+     * @param $ids
+     * @return array
+     */
+    protected function transIdsToArr($ids)
+    {
+        if (!$this->isIdsArr) {
+            $idsArr = array_unique(array_filter(explode($this->explodeBy, $ids)));
+        } else {
+            $idsArr = $ids;
+        }
+        return $idsArr;
     }
 }
---
+++ actions/web/crud/BatchOperateAction.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace kriss\actions\web\crud;
+
+use kriss\actions\helper\ActionTools;
+use kriss\components\MessageAlert;
+use kriss\traits\WebControllerTrait;
+
+class BatchOperateAction extends \kriss\actions\rest\crud\BatchOperateAction
+{
+    use WebControllerTrait;
+
+    /**
+     * @var string
+     */
+    public $idsAttribute = 'keys';
+    /**
+     * @var bool
+     */
+    public $isIdsArr = true;
+    /**
+     * @var string
+     */
+    public $operateMsg = '批量操作';
+    /**
+     * @var string|array
+     */
+    public $successRedirect;
+
+    public function run()
+    {
+        $ids = $this->getRequestIds();
+        if (!$ids) {
+            MessageAlert::error("{$this->idsAttribute} 必传");
+            return $this->redirectPrevious();
+        }
+        $idsArr = $this->transIdsToArr($ids);
+
+        $result = ActionTools::invokeClassMethod($this->controller, $this->doMethod, $idsArr);
+
+        $totalCount = count($idsArr);
+        if (is_int($result)) {
+            if ($result == $totalCount) {
+                MessageAlert::success("{$this->operateMsg}成功，共处理:{$result}数据");
+            } else {
+                $noSolveCount = $totalCount - $result;
+                MessageAlert::warning("{$this->operateMsg}成功，共处理:{$result}条数据，未处理{$noSolveCount}条数据");
+            }
+        } elseif ($result === false || is_string($result)) {
+            if ($result === false) {
+                $result = '请联系管理员';
+            }
+            MessageAlert::error("{$this->operateMsg}失败，{$result}");
+        }
+
+        return $this->redirectPrevious();
+    }
+
+    /**
+     * 跳转到前一个页面
+     */
+    public function redirectPrevious()
+    {
+        return $this->actionPreviousRedirect($this->controller);
+    }
+}
