db9867f6ffd632943cfbf9e7fefff48407d214b4
kriss <462679766@qq.com>
2018-09-03 11:27:55 +0800

    feat: add ExportMenu

---
+++ widgets/ExportMenu.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace kriss\widgets;
+
+use Yii;
+use yii\base\Exception;
+use yii\base\Widget;
+use yii\data\BaseDataProvider;
+use yii\grid\ActionColumn;
+use yii\grid\CheckboxColumn;
+use yii\grid\RadioButtonColumn;
+use yii\grid\SerialColumn;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii2tech\csvgrid\CsvGrid;
+
+class ExportMenu extends Widget
+{
+    public $dataProvider;
+
+    public $columns = [];
+
+    public $skipColumnClass = [
+        ActionColumn::class, CheckboxColumn::class, RadioButtonColumn::class, SerialColumn::class,
+    ];
+
+    public $batchSize = 200;
+
+    public $csvGridConfig;
+
+    public $savePath;
+
+    public $sendName = 'export.csv';
+
+    public $exportMenuLabel = '导出';
+
+    public $exportMenuOptions = ['class' => 'btn btn-primary'];
+
+    public $exportPostParam = 'export-menu-export';
+
+    private $triggeredExport = false;
+
+    public function init()
+    {
+        if (!class_exists('yii2tech\csvgrid\CsvGrid')) {
+            throw new Exception('must install yii2tech/csv-grid');
+        }
+        parent::init();
+        $this->triggeredExport = Yii::$app->request->post($this->exportPostParam, false);
+    }
+
+    public function run()
+    {
+        if (!$this->triggeredExport) {
+            $this->renderExportMenu();
+            return;
+        }
+        $this->export();
+    }
+
+    protected function renderExportMenu()
+    {
+        echo Html::beginForm();
+        echo Html::hiddenInput($this->exportPostParam, true);
+        echo Html::submitInput($this->exportMenuLabel, $this->exportMenuOptions);
+        echo Html::endForm();
+    }
+
+    protected function export()
+    {
+        Yii::trace('Export Start', __CLASS__);
+
+        $config = [
+            'csvFileConfig' => [
+                'writeBom' => true, // 解决utf8中文乱码问题
+            ],
+            'batchSize' => $this->batchSize,
+            'columns' => $this->getColumns(),
+        ];
+        if ($this->csvGridConfig) {
+            $config = ArrayHelper::merge($config, $this->csvGridConfig);
+        }
+
+        /** @var BaseDataProvider $dataProvider */
+        $dataProvider = $this->dataProvider;
+        if ($dataProvider->hasProperty('pagination')) {
+            $dataProvider->pagination->pageSize = $config['batchSize'];
+        }
+        $config['dataProvider'] = $dataProvider;
+
+        $exporter = new CsvGrid($config);
+        $exportResult = $exporter->export();
+        if ($this->savePath) {
+            $exportResult->saveAs($this->savePath);
+        } else {
+            $exportResult->send($this->sendName);
+        }
+
+        Yii::trace('Export End', __CLASS__);
+    }
+
+    protected function getColumns()
+    {
+        $columns = [];
+        foreach ($this->columns as $column) {
+            if (is_array($column) && isset($column['class'])) {
+                $skip = false;
+                foreach ($this->skipColumnClass as $columnClass) {
+                    if (is_subclass_of($column['class'], $columnClass)) {
+                        $skip = true;
+                        break;
+                    }
+                }
+                if ($skip) {
+                    continue;
+                }
+            }
+            $columns[] = $column;
+        }
+        return $columns;
+    }
+}
