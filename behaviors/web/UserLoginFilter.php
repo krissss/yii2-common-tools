87e43dd4dee860e07786f8be8706d7f4b3a50d36
kriss <462679766@qq.com>
2019-09-27 15:04:15 +0800

    feat: add rememberUrl if no login

--- behaviors/web/UserLoginFilter.php
+++ behaviors/web/UserLoginFilter.php
@@ -4,6 +4,7 @@ namespace kriss\behaviors\web;
 
 use Yii;
 use yii\base\ActionFilter;
+use yii\helpers\Url;
 
 /**
  * 用户登录过滤
@@ -15,9 +16,20 @@ class UserLoginFilter extends ActionFilter
      * @var array
      */
     public $loginUrl = ['/site/login'];
+    /**
+     * @var bool
+     */
+    public $rememberUrl = true;
+    /**
+     * @var string
+     */
+    public $rememberUrlName = null;
 
     public function beforeAction($action)
     {
+        if ($this->rememberUrl && Yii::$app->request->isGet) {
+            Url::remember('', $this->rememberUrlName);
+        }
         if (Yii::$app->user->isGuest) {
             Yii::$app->getResponse()->redirect($this->loginUrl);
             return false;
